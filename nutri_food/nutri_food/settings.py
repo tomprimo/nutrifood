e637eb3d893a037b60e11b2a57ac1ac50c11fb4d
tmochet <tmochet@eutelsat.com>
2023-06-27 10:24:07 +0200

    postgre config

--- nutri_food/nutri_food/settings.py
+++ nutri_food/nutri_food/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'nutri_food.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nutrifood_db',
+        'USER': 'teo',
+        'PASSWORD': '2212',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
